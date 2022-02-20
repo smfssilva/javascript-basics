7596bba1f3870079369a48dd3661796067918056
smfssilva <smfssilva@yahoo.com>
2022-02-19 23:42:59 -0300

    Color Flipper - Single Javascript

--- secao-6/01-color-flipper/setup/app.js
+++ secao-6/01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  const randomNumber = Math.floor(Math.random() * 3)
+  document.body.style.background = colors[randomNumber]
+  color.textContent = colors[randomNumber]
+})
