86d5943510dabb5b35d5e82d629a7cde192a351a
smfssilva <smfssilva@yahoo.com>
2022-02-19 22:41:37 -0300

    Event-Scroll

---
+++ secao-5/34-Event-Scroll/app.js
@@ -0,0 +1,8 @@
+const link = document.querySelector('a')
+link.style.background = 'green';
+link.style.display = 'block';
+link.style.marginTop = '1000px'
+
+window.addEventListener('scroll', function () {
+  console.log('hello');
+})
